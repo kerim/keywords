f7c9d96aa930655f034645d391ac0afe858f1b89
P. Kerim Friedman <kerim.mail@oxus.net>
2015-09-23 22:45:12 +0800

    js approach

---
+++ js/js-video.js
@@ -0,0 +1,17 @@
+.js-video {
+  height: 0;
+  padding-top: 25px;
+  padding-bottom: 67.5%;
+  margin-bottom: 10px;/Users/PKF_MacBook/Documents/Git/keywords/js
+  
+  position: relative;
+  overflow: hidden;
+}
+
+.js-video.widescreen {
+  padding-bottom: 56.34%;
+}
+
+.js-video.vimeo {
+  padding-top: 0;
+  }
